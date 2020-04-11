43711f5532213a0ad2b156c3dd501d1a692a84f6
Andrew Adhigunarsa <adhigunarsa@192-168-1-105.tpgi.com.au>
2020-04-12 01:04:43 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gardening/precache-manifest.724eba17fcbc1d318ea5d39f0a600c73.js"
+  "thesydneygardener.com.au/precache-manifest.2f629499e95302b0ff79649bd35bb7f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gardening/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("thesydneygardener.com.au/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
